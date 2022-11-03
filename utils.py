51b46775008ca70834e9ce858daad227e0ff3ee5
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2022-11-03 12:18:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -386,8 +386,8 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://api.shareus.in/shortlink'
-    params = {'api': 'TFmLgA305LAYoUkU7OBjFk0OcCkr1',
+    url = f'https://easysky.in/api'
+    params = {'api': '56cb94ebee3c5ee7d1c84808168f60acdfc50657',
               'url': link,
               }
 
