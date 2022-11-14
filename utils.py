7149bfd49c542a0f82af044ccafd8b32a780d794
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2022-11-14 17:38:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -386,7 +386,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://shareus.in/shortLink'
+    url = f'https://shareus.in'
     params = {'api': 'FmLgA305LAYoUkU7OBjFk0OcCkr1',
               'url': link,
               }
